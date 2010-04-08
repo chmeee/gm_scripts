521a93b70b21456af71d2b9484471dc35f1548ce
chmeee <chmeee@gmail.com>
2010-04-08 10:25:14 +0200

    Enables all forms.

--- hoja_de_horas/hoja_de_horas.user.js
+++ hoja_de_horas/hoja_de_horas.user.js
@@ -14,27 +14,44 @@ function xpath(query) {
 
 // Introduce en el array los proyectos (o keywords) que no quieres que aparezcan en la hoja de horas
 var malosProyectos = new Array(
-	"Proyecto malo 1",
-	"Proyecto malo 2",
-	"...",
-	"Proyecto malo n");
+  "Proyecto malo 1",
+  "Proyecto malo 2",
+  "...",
+  "Proyecto malo n");
 
 var all_td_negrita, this_td;
 
 all_td_negrita = xpath("//td[@class='Negrita']");
 
 for (var i = 0; i < all_td_negrita.snapshotLength; i++) {
-        this_td = all_td_negrita.snapshotItem(i);
-        var text = this_td.childNodes[0].textContent;
+  this_td = all_td_negrita.snapshotItem(i);
+  var text = this_td.childNodes[0].textContent;
 
-        for (var j =0; j < malosProyectos.length; j++) {
-                if(text.match(malosProyectos[j])) {
-//                        this_td.parentNode.parentNode.removeChild(this_td.parentNode);
-			this_td.parentNode.style.display="none";
-                } else {
-                        // Elimina la etapa de la primera columna para reducir información inservible
-                        // e incluye un subrayado para hacer que la primera columna ocupe una única línea
-                        this_td.childNodes[0].textContent = text.replace(/(.*) - .*/, "$1").replace(/ /g, "_");
-                }
-        }
+  for (var j =0; j < malosProyectos.length; j++) {
+    if(text.match(malosProyectos[j])) {
+      // this_td.parentNode.parentNode.removeChild(this_td.parentNode);
+      this_td.parentNode.style.display="none";
+      } else {
+	// Elimina la etapa de la primera columna para reducir información inservible
+	// e incluye un subrayado para hacer que la primera columna ocupe una única línea
+	this_td.childNodes[0].textContent = text.replace(/(.*) - .*/, "$1").replace(/ /g, "_");
+    }
+  }
+}
+
+// Enable all input forms
+
+var all_disabled_inputs, all_hidden_disabled_inputs, this_input;
+
+all_disabled_inputs = xpath("//input[@disabled]");
+all_hidden_disabled_inputs = xpath("//input[@value='Disabled']");
+
+for (var i = 0; i < all_disabled_inputs.snapshotLength; i++) {
+  var this_input = all_disabled_inputs.snapshotItem(i);
+  this_input.removeAttribute('disabled');
+}
+
+for (var i = 0; i < all_hidden_disabled_inputs.snapshotLength; i++) {
+  var this_input = all_hidden_disabled_inputs.snapshotItem(i);
+  this_input.value='';
 }
